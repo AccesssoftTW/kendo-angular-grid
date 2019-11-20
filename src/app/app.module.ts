e40d594842fe586800ed901ebb63ed89599d5e9c
Winston 盧文松 <aa71435723@gmail.com>
2019-11-20 14:13:11 +0800

    新增grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
+import { EditService } from './edit.service';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    HttpClientModule,
+    HttpClientJsonpModule,
+    ReactiveFormsModule,
+    BrowserModule,
+    GridModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    {
+      deps: [HttpClient],
+      provide: EditService,
+      useFactory: (jsonp: HttpClient) => () => new EditService(jsonp)
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
